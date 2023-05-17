c4823c0c1b8720e2c21c6e77d0d0939d591b8ac2
Planifyit <133891801+Planifyit@users.noreply.github.com>
2023-05-17 22:06:17 +0200

    Update CalculatorWidget.js

--- CalculatorWidget.js
+++ CalculatorWidget.js
@@ -1,28 +1,78 @@
-(function () {
+((function () {
     let tmpl = document.createElement('template');
     tmpl.innerHTML = `
         <style>
-            /* Add your CSS styling here */
+            .calculator {
+                display: flex;
+                flex-direction: column;
+                width: 200px;
+                background-color: #f0f0f0;
+                border-radius: 4px;
+                padding: 10px;
+            }
+
+            .display {
+                margin-bottom: 10px;
+                background-color: #fff;
+                height: 30px;
+                border: none;
+                padding: 5px;
+                text-align: right;
+                border-radius: 4px;
+            }
+
+            .buttons {
+                display: grid;
+                grid-template-columns: repeat(4, 1fr);
+                grid-gap: 5px;
+            }
+
+            .buttons > button {
+                height: 30px;
+                border: none;
+                color: #fff;
+                border-radius: 4px;
+            }
+
+            .buttons > button:active {
+                transform: scale(0.95);
+            }
+
+            .buttons > button:nth-child(-n+3) {
+                background-color: #f77;
+            }
+
+            .buttons > button:nth-child(n+4):nth-child(-n+6) {
+                background-color: #7f7;
+            }
+
+            .buttons > button:nth-child(n+7):nth-child(-n+9) {
+                background-color: #77f;
+            }
+
+            .buttons > button:nth-child(n+10) {
+                background-color: #777;
+            }
         </style>
         <div class="calculator">
             <input type="text" class="display" disabled>
             <div class="buttons">
-                <button>1</button>
-                <button>2</button>
-                <button>3</button>
-                <button>+</button>
-                <button>4</button>
-                <button>5</button>
-                <button>6</button>
-                <button>-</button>
                 <button>7</button>
                 <button>8</button>
                 <button>9</button>
                 <button>*</button>
-                <button>.</button>
-                <button>0</button>
+                <button>4</button>
+                <button>5</button>
+                <button>6</button>
                 <button>/</button>
+                <button>1</button>
+                <button>2</button>
+                <button>3</button>
+                <button>-</button>
+                <button>0</button>
+                <button>.</button>
                 <button>=</button>
+                <button>+</button>
             </div>
         </div>
     `;
