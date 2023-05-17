2d28db5db0e197ed3c5b4bbfdee866b81ad12d4a
Planifyit <133891801+Planifyit@users.noreply.github.com>
2023-05-17 19:26:53 +0200

    Update WeatherWidget.js

--- WeatherWidget.js
+++ WeatherWidget.js
@@ -1,24 +1,31 @@
 (function () {
     let tmpl = document.createElement('template');
     tmpl.innerHTML = 
-    `<div id="weatherInfo">Loading...</div>`;  
-        
-    class WeatherWidget extends HTMLElement {
+    `<button type="button" id="myBtn">Helper Button</button>` ;   
+   
+    class PerformanceHelp extends HTMLElement {
         constructor() {
             super();
-            this.attachShadow({mode: "open"});
-            this.shadowRoot.appendChild(tmpl.content.cloneNode(true));
-            this.loadWeatherData();
+            this.init();           
         }
 
-        async loadWeatherData() {
-            let weatherDiv = this.shadowRoot.querySelector("#weatherInfo");
-            // Call the API and update the weatherDiv's innerHTML with the real-time data
-            // For example, if using OpenWeatherMap API:
-            // const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=London&appid=your_api_key');
-            // const data = await response.json();
-            // weatherDiv.innerHTML = `Current temperature in London: ${data.main.temp}`;
+        init() {            
+              
+            let shadowRoot = this.attachShadow({mode: "open"});
+            shadowRoot.appendChild(tmpl.content.cloneNode(true));
+            this.addEventListener("click", event => {
+            var event = new Event("onClick");
+            this.fireChanged();           
+            this.dispatchEvent(event);
+            });           
         }
+
+        fireChanged() {
+            console.log("OnClick Triggered");     
+            
+        }        
+        
     }
-    customElements.define('weather-widget', WeatherWidget);
+
+    customElements.define('custom-button', PerformanceHelp);
 })();
