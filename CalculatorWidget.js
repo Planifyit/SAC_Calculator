5eef87fa73a740d66d4e868c1fc8341656cec8e9
Planifyit <133891801+Planifyit@users.noreply.github.com>
2023-05-18 12:44:21 +0200

    Update CalculatorWidget.js

--- CalculatorWidget.js
+++ CalculatorWidget.js
@@ -51,7 +51,7 @@
 
     .buttons > button:nth-child(n+5):nth-child(-n+8),
     .buttons > button:nth-child(n+9):nth-child(-n+12) {
-        background-color: indigo; 
+        background-color: indigo blue ; 
     }
 
     .buttons > button:nth-child(n+13):nth-child(-n+16) {
@@ -72,23 +72,23 @@
 
             <div class="buttons">
         <button>%</button>
-        <button>7</button>
-        <button>4</button>
-        <button>1</button>
         <button>C</button>
-        <button>8</button>
-        <button>5</button>
-        <button>2</button>
-        <button>0</button>
         <button><</button>
-        <button>9</button>
-        <button>6</button>
-        <button>3</button>
-        <button>.</button>
         <button>/</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
         <button>*</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
         <button>-</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
         <button>+</button>
+        <button>0</button>
+        <button>.</button>
         <button>=</button>
        
             </div>
