a73f376220e419dc1f23822feb46b4ea4e441d84
Planifyit <133891801+Planifyit@users.noreply.github.com>
2023-05-18 12:36:22 +0200

    Update CalculatorWidget.js

--- CalculatorWidget.js
+++ CalculatorWidget.js
@@ -67,24 +67,25 @@
 
             <div class="buttons">
         <button>%</button>
+        <button>7</button>
+        <button>4</button>
+        <button>1</button>
         <button>C</button>
+        <button>8</button>
+        <button>5</button>
+        <button>2</button>
+        <button>0</button>
         <button><</button>
-               <button>7</button>
-                <button>8</button>
-                <button>9</button>
-                <button>*</button>
-                <button>4</button>
-                <button>5</button>
-                <button>6</button>
-                <button>/</button>
-                <button>1</button>
-                <button>2</button>
-                <button>3</button>
-                <button>-</button>
-                <button>0</button>
-                <button>.</button>
-                <button>=</button>
-                <button>+</button>
+        <button>9</button>
+        <button>6</button>
+        <button>3</button>
+        <button>.</button>
+        <button>/</button>
+        <button>*</button>
+        <button>-</button>
+        <button>+</button>
+        <button>=</button>
+       
             </div>
         </div>
     `;
