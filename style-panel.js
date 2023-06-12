49c803e33051cebfaf0860ccb42eddba1ca984bf
planifyit.com <contact@planifyit.com>
2023-06-12 11:08:28 +0000

    Update style-panel.js

--- style-panel.js
+++ style-panel.js
@@ -6,8 +6,12 @@
 				<legend>Calculator Properties</legend>
 				<table>
 					<tr>
-						<td>Number Color</td>
-						<td><input id="styling_number_color" type="text" size="40" maxlength="40"></td>
+						<td>Equal Button Color</td>
+						<td><input id="styling_equal_color" type="text" size="40" maxlength="40"></td>
+					</tr>
+					<tr>
+						<td>Clear Button Color</td>
+						<td><input id="styling_clear_color" type="text" size="40" maxlength="40"></td>
 					</tr>
 				</table>
 				<input type="submit" style="display:none;">
@@ -28,18 +32,27 @@
 			this.dispatchEvent(new CustomEvent("propertiesChanged", {
 					detail: {
 						properties: {
-							numberColor: this.numberColor
+							equalColor: this.equalColor,
+							clearColor: this.clearColor
 						}
 					}
 			}));
 		}
 
-		set numberColor(newColor) {
-			this._shadowRoot.getElementById("styling_number_color").value = newColor;
+		set equalColor(newColor) {
+			this._shadowRoot.getElementById("styling_equal_color").value = newColor;
 		}
 
-		get numberColor() {
-			return this._shadowRoot.getElementById("styling_number_color").value;
+		get equalColor() {
+			return this._shadowRoot.getElementById("styling_equal_color").value;
+		}
+
+		set clearColor(newColor) {
+			this._shadowRoot.getElementById("styling_clear_color").value = newColor;
+		}
+
+		get clearColor() {
+			return this._shadowRoot.getElementById("styling_clear_color").value;
 		}
 	}
 
